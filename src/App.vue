7cee5937f1cf84925a85e1fbc968f56d0907a328
uzcoder-oo8 <sardortoxtoshov@gmail.com>
2024-05-20 12:03:08 +0500

    vue-counter

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container">
-    <h1>Counter : {{ counter }}</h1>
+    <h1 class="head">Counter : {{ counter }}</h1>
     <div class="d-flex">
       <button @click="onIncrement" class="btn btn-success">Add</button>
       <button @click="onReflesh">Reflesh</button>
-      <button @click="onDecrement" class="btn btn-danger ml-">Delete</button>
+      <button @click="onDecrement" class="btn btn-danger">Delete</button>
     </div>
   </div>
 </template>
@@ -28,4 +28,39 @@ export default {
   },
 };
 </script>
-<style></style>
+<style>
+.head {
+  text-align: center;
+  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+    "Lucida Sans", Arial, sans-serif;
+}
+.d-flex {
+  width: 340px;
+  text-align: center;
+  margin: auto auto;
+}
+button {
+  width: 100px;
+  border: 1px solid black;
+  padding: 10px;
+  border-radius: 20px;
+}
+
+button:nth-child(1) {
+  background-color: teal;
+  font-size: 16px;
+  color: white;
+}
+button:nth-child(2) {
+  margin-left: 20px;
+  background-color: gold;
+  font-size: 16px;
+  color: white;
+}
+button:nth-child(3) {
+  margin-left: 20px;
+  background-color: red;
+  font-size: 16px;
+  color: white;
+}
+</style>
