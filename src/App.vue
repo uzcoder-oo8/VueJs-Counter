74b4b567ff1094ab83561bebb47961fba12e7f60
uzcoder-oo8 <sardortoxtoshov@gmail.com>
2024-05-20 10:20:52 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <h1>Counter : {{ counter }}</h1>
+    <div class="d-flex">
+      <button @click="onIncrement" class="btn btn-success">Add</button>
+      <button @click="onReflesh">Reflesh</button>
+      <button @click="onDecrement" class="btn btn-danger ml-">Delete</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    onIncrement() {
+      this.counter += 1;
+    },
+    onDecrement() {
+      this.counter -= 1;
+    },
+    onReflesh() {
+      this.counter = 0;
+    },
+  },
+};
+</script>
+<style></style>
